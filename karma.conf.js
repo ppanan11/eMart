0249758346a8a655b6c7e9afc143b9a53ff93d26
ppanan11 <ppanan@cn.ibm.com>
2020-05-04 17:26:45 +0800

    jasmine test

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: 'src\app\app.component.spec.ts',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,18 @@
 import {TestBed, async} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,HttpClientModule
       ],
       declarations: [
         AppComponent
       ],
+      providers:[HttpClient]
     }).compileComponents();
   }));
 
@@ -26,10 +28,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('eMart');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('eMart app is running!');
-  });
+
 });
